d2dfeef268681d1b0333d31500112df5db908f90
Om Sharma <77867638+KILLERTIAN@users.noreply.github.com>
2022-09-21 21:17:17 +0530

    Responsive changes part-4

--- main.js
+++ main.js
@@ -1,53 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
     let controller = new ScrollMagic.Controller();
-
+    let t1 = gsap.timeline({ paused: true, reverse: true });
     let timeline = new TimelineMax();
-    timeline
-    .from('.section_1_01', 4, {
+    t1.play();
+    t1.from(".navbar .logo",4, {
         y: -100,
         x: -150,
         ease: Power3.easeInOut
-    })
-    .from('.section_1_02', 4, {
-        y: -150,
-        x: -225,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_03', 4, {
-        y: -80,
-        x: -100,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_04', 4, {
-        y: -120,
-        x: -170,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_05', 4, {
-        y: -60,
-        x: -150,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_06', 4, {
-        y: -50,
-        x: -220,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_07', 4, {
-        y: -50,
-        x: -150,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_08', 4, {
-        y: 10,
-        x: -200,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .from('.section_1_09', 4, {
-        y: 40,
-        x: -170,
-        ease: Power3.easeInOut
-    }, '-=4')
+    }
+    );
+    timeline
+        .from('.section_1_01', 4, {
+            y: -100,
+            x: -150,
+            ease: Power3.easeInOut
+        })
+        .from('.section_1_02', 4, {
+            y: -150,
+            x: -225,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_03', 4, {
+            y: -80,
+            x: -100,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_04', 4, {
+            y: -120,
+            x: -170,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_05', 4, {
+            y: -60,
+            x: -150,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_06', 4, {
+            y: -50,
+            x: -220,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_07', 4, {
+            y: -50,
+            x: -150,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_08', 4, {
+            y: 10,
+            x: -200,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .from('.section_1_09', 4, {
+            y: 40,
+            x: -170,
+            ease: Power3.easeInOut
+        }, '-=4')
 
     let scene = new ScrollMagic.Scene({
         triggerElement: '.first-section',
@@ -55,15 +62,15 @@ document.addEventListener('DOMContentLoaded', () => {
         triggerHook: 0,
         offset: '300'
     })
-    .setTween(timeline)
-    .setPin('.first-section')
-    .addTo(controller);
+        .setTween(timeline)
+        .setPin('.first-section')
+        .addTo(controller);
 
     let timeline2 = new TimelineMax();
     timeline2
-    .to('.top .image-container', 4, {
-        height: 0
-    });
+        .to('.top .image-container', 4, {
+            height: 0
+        });
 
     let scene2 = new ScrollMagic.Scene({
         triggerElement: '.second-section',
@@ -71,36 +78,36 @@ document.addEventListener('DOMContentLoaded', () => {
         triggerHook: 0,
         offset: '100'
     })
-    .setTween(timeline2)
-    .setPin('.second-section')
-    .addTo(controller);
+        .setTween(timeline2)
+        .setPin('.second-section')
+        .addTo(controller);
 
     let timeline3 = new TimelineMax();
     timeline3
-    .to('.section_3_01', 4, {
-        y: -250,
-        ease: Power3.easeInOut
-    })
-    .to('.section_3_02', 4, {
-        y: -200,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .to('.section_3_03', 4, {
-        y: -100,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .to('.section_3_04', 4, {
-        y: 0,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .to('.section_3_05', 4, {
-        y: 150,
-        ease: Power3.easeInOut
-    }, '-=4')
-    .to('.section_3_06', 4, {
-        y: 250,
-        ease: Power3.easeInOut
-    }, '-=4')
+        .to('.section_3_01', 4, {
+            y: -250,
+            ease: Power3.easeInOut
+        })
+        .to('.section_3_02', 4, {
+            y: -200,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_03', 4, {
+            y: -100,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_04', 4, {
+            y: 0,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_05', 4, {
+            y: 150,
+            ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_06', 4, {
+            y: 250,
+            ease: Power3.easeInOut
+        }, '-=4')
 
     let scene3 = new ScrollMagic.Scene({
         triggerElement: '.third-section',
@@ -108,24 +115,24 @@ document.addEventListener('DOMContentLoaded', () => {
         triggerHook: 0,
         offset: '200'
     })
-    .setTween(timeline3)
-    .setPin('.third-section')
-    .addTo(controller);
+        .setTween(timeline3)
+        .setPin('.third-section')
+        .addTo(controller);
 
     let timeline4 = new TimelineMax();
     timeline4
-    .to('.section_4_01', 4, {
-        autoAlpha: 0
-    })
-    .from('.section_4_02', 4, {
-        autoAlpha: 0
-    }, '-=4')
-    .from('.section_4_03', 4, {
-        autoAlpha: 0
-    })
-    .from('.section_4_04', 4, {
-        autoAlpha: 0
-    })
+        .to('.section_4_01', 4, {
+            autoAlpha: 0
+        })
+        .from('.section_4_02', 4, {
+            autoAlpha: 0
+        }, '-=4')
+        .from('.section_4_03', 4, {
+            autoAlpha: 0
+        })
+        .from('.section_4_04', 4, {
+            autoAlpha: 0
+        })
 
     let scene4 = new ScrollMagic.Scene({
         triggerElement: '.forth-section',
@@ -133,9 +140,9 @@ document.addEventListener('DOMContentLoaded', () => {
         triggerHook: 0,
         offset: '200'
     })
-    .setTween(timeline4)
-    .setPin('.forth-section')
-    .addTo(controller);
+        .setTween(timeline4)
+        .setPin('.forth-section')
+        .addTo(controller);
 })
 // to scroll website to top when refreshed
 if (history.scrollRestoration) {
@@ -146,7 +153,7 @@ if (history.scrollRestoration) {
     }
 }
 
-function menuToggle(){
+function menuToggle() {
     const toggleMenu = document.querySelector('.toggleMenu');
     const navigation = document.querySelector('.navigation');
     toggleMenu.classList.toggle('active');
